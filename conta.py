eb803a2e14dbe764bc07fdf9c2a71e9ba9661d74
jhow <jhow.merisse@hotmail.com>
2018-08-26 19:00:35 -0300

    metodos estaticos e decorators

--- conta.py
+++ conta.py
@@ -23,6 +23,10 @@ class Conta:
     def limite(self, limite):
         self.__limite = limite
 
+    def __pode_sacar(self, valor_saque):
+        saldo_disponivel = self.__saldo + self.__limite
+        return saldo_disponivel >= valor_saque
+
     def extrato(self):
         print("O saldo é de {}".format(self.__saldo))
 
@@ -30,8 +34,19 @@ class Conta:
         self.__saldo += valor
 
     def saca(self, valor):
-        self.__saldo -= valor
+        if self.__pode_sacar(valor):
+            self.__saldo -= valor
+        else:
+            print("saldo insuficiente")
 
     def tranfere(self, valor, destino):
         self.saca(valor)
         destino.deposita(valor)
+
+    @staticmethod
+    def codigo_banco():
+        return "001"
+
+    @staticmethod
+    def codigos_bancos():
+        return {'BB':'001', 'Caixa':'104', 'Bradesco':'237'}
