491efa1ce020b00e2a10c9d7a85f2ca042b8ea64
jhow <jhow.merisse@hotmail.com>
2018-08-26 17:20:55 -0300

    update

--- conta.py
+++ conta.py
@@ -2,7 +2,20 @@ class Conta:
 
     def __init__(self, numero, titular, saldo, limite):
         print("Construindo objeto ... {}".format(self))
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+
+    def extrato(self):
+        print("O saldo é de {}".format(self.__saldo))
+
+    def deposita(self, valor):
+        self.__saldo += valor
+
+    def saca(self, valor):
+        self.__saldo -= valor
+
+    def tranfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
