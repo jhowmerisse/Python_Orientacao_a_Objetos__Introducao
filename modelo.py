49e0c3bac3103cc685fa8b4132668aebe586c102
Jhovani Cesar Merisse <Jhovani.Merisse@MIDGARD.LOCAL>
2018-09-18 13:42:30 -0300

    criando classes e encapsulando comportamentos

---
+++ modelo.py
@@ -0,0 +1,55 @@
+class Filme:
+    def __init__(self, nome, ano, duracao):
+        self.__nome = nome.title()
+        self.ano = ano
+        self.duracao = duracao
+        self.__likes = 0
+
+    @property
+    def likes(self):
+        return self.__likes
+
+    def dar_like(self):
+        self.__likes += 1
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome.title()
+
+
+class Serie:
+    def __init__(self, nome, ano, temporadas):
+        self.__nome = nome.title()
+        self.ano = ano
+        self.temporadas = temporadas
+        self.__likes = 0
+
+    # getter
+    @property
+    def likes(self):
+        return self.__likes
+
+    def dar_like(self):
+        self.__likes += 1
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome.title()
+
+
+vingadores = Filme('vingadores - guerra infinita', 2018, 160)
+vingadores.dar_like()
+print(f'Nome: {vingadores.nome} - Ano {vingadores.ano} - duracao: {vingadores.duracao} - Likes: {vingadores.likes}')
+
+atlanta = Serie('atlanta', 2016, 2)
+atlanta.dar_like()
+atlanta.dar_like()
+print(f'Nome: {atlanta.nome} - Ano {atlanta.ano} - temporadas: {atlanta.temporadas} - Likes: {atlanta.likes}')
